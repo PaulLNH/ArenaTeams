0c04496525051329703464334cbfa068e2ee8584
plaird730 <plaird730@gmail.com>
2018-10-22 13:06:37 -0400

    Initial commit

---
+++ ArenaTeams.lua
@@ -0,0 +1,20 @@
+message('Welcome to the Arena Teams addon, bringing back all the fun of arena teams without the restrictions!')
+
+--[[
+    OVERVIEW
+
+    *GOAL*: Bind player and arena partners together with a "Team Name", Allow users to create their own arena team names that will display for all users of the addon, track arena team rating (W/L - possibly some sort or team rating and % win rate for each team), track team compositions and display a "leaderboard"
+
+    # Arena Teams
+        # Combines the character
+    
+    # Rating Change
+        # Record current session arena rating
+        # Display rating change since start of current session
+--]]
+
+--[[
+    STEPS
+
+    - Load addon and display arena team at the top
+--]]
---
+++ README.md
@@ -0,0 +1,13 @@
+# ArenaTeams
+
+ArenaTeams is an addon that brings back all the fun of arena teams without the restrictions.
+
+Open a ticket to report any issues:
+https://wow.curseforge.com/projects/arenateams/issues
+
+Submit a pull request:
+https://github.com/paullnh/arenateams/pulls
+
+## v0.1
+
+* Alpha build
